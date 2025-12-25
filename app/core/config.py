64b8fcef706f832f33f9b1b6dbac3ff1204872ff
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2025-12-26 02:33:28 +0530

    create: docker db and env vars

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,16 @@ class Settings(BaseSettings):
     app_name: str = "FastAPI Todo Backend"
     environment: str = Field(default="development")
 
+    db_host: str
+    db_port: int
+    db_user: str
+    db_password: str
+    db_name: str
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
